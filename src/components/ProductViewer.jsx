0b2c0644cfb174ee2c6473ea2a9dd1c3a513b655
Peiris M. H. C. <154981814+IT22194930@users.noreply.github.com>
2025-10-12 18:51:43 +0530

    feat: integrate GSAP for animations and add ModelSwitcher component

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,11 +3,16 @@ import clsx from 'clsx';
 import { Canvas } from '@react-three/fiber';
 import { Box, OrbitControls } from '@react-three/drei';
 import MacbookModel14 from './models/Macbook-14';
-import StudioLights from './models/StudioLights';
+import StudioLights from './three/StudioLights';
+
+import ModelSwitcher from './three/ModelSwitcher';
+import { useMediaQuery } from 'react-responsive';
 
 const ProductViewer = () => {
 
    const {color, scale, setColor, setScale} = useMacbookStore();
+
+   const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
 
   return (
     <section id="product-viewer">
@@ -35,10 +40,7 @@ const ProductViewer = () => {
 
       <Canvas id='canvas' camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
          <StudioLights />
-
-         <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
-
-         <OrbitControls enableZoom={false} />
+         <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
       </Canvas>
     </section>
   )
